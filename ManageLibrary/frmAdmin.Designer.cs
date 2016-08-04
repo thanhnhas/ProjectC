41b3ad6d2ba379ac474fed9e505db43bc030d4fe
hoangtran <hoangtran@HOANGTLTSE61587>
2016-08-04 07:20:18 +0700

    test

--- ManageLibrary/frmAdmin.Designer.cs
+++ ManageLibrary/frmAdmin.Designer.cs
@@ -36,51 +36,56 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 46);
+            this.button1.Location = new System.Drawing.Point(63, 37);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(212, 49);
+            this.button1.Size = new System.Drawing.Size(159, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "Quản lý";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(84, 210);
+            this.button3.Location = new System.Drawing.Point(63, 171);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(212, 51);
+            this.button3.Size = new System.Drawing.Size(159, 41);
             this.button3.TabIndex = 2;
             this.button3.Text = "Nhận trả sách";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(84, 125);
+            this.button4.Location = new System.Drawing.Point(63, 102);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(212, 49);
+            this.button4.Size = new System.Drawing.Size(159, 40);
             this.button4.TabIndex = 3;
             this.button4.Text = "Cho mượn sách";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(84, 298);
+            this.button2.Location = new System.Drawing.Point(63, 242);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(212, 49);
+            this.button2.Size = new System.Drawing.Size(159, 40);
             this.button2.TabIndex = 4;
             this.button2.Text = "Thoát";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // frmAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 462);
+            this.ClientSize = new System.Drawing.Size(321, 358);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmAdmin";
-            this.Text = "Thủ thư";
+            this.Text = "dd";
             this.ResumeLayout(false);
 
         }
