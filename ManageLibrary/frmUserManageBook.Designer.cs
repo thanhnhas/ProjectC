ae1a65593a773bddf6a9ff37603b2ab39ffedef7
hoangtran <hoangtran@HOANGTLTSE61587>
2016-08-04 07:38:20 +0700

    ko sua gi ca

--- ManageLibrary/frmUserManageBook.Designer.cs
+++ ManageLibrary/frmUserManageBook.Designer.cs
@@ -37,38 +37,42 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(45, 46);
+            this.dataGridView1.Location = new System.Drawing.Point(34, 37);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(811, 310);
+            this.dataGridView1.Size = new System.Drawing.Size(608, 252);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(229, 397);
+            this.button1.Location = new System.Drawing.Point(172, 323);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 42);
+            this.button1.Size = new System.Drawing.Size(83, 34);
             this.button1.TabIndex = 1;
             this.button1.Text = "Gia Hạn";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(473, 397);
+            this.button2.Location = new System.Drawing.Point(412, 323);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 42);
+            this.button2.Size = new System.Drawing.Size(80, 34);
             this.button2.TabIndex = 2;
             this.button2.Text = "Thoát";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // frmUserManageBook
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 475);
+            this.ClientSize = new System.Drawing.Size(675, 386);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmUserManageBook";
             this.Text = "Quản lý sách đã mượn";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
