f39de5efed774546d885ea9b5fc50a2967d05bcb
Administrator <Administrator@HOANGTLTSE61587>
2016-08-16 23:54:20 +0700

    Last Fix Search

--- ManageLibrary/frmSearch.Designer.cs
+++ ManageLibrary/frmSearch.Designer.cs
@@ -36,6 +36,7 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -43,20 +44,24 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Controls.Add(this.txtSearch);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(550, 75);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(412, 70);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Nhập thông tin";
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(437, 30);
+            this.btnSearch.Location = new System.Drawing.Point(328, 24);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(94, 23);
+            this.btnSearch.Size = new System.Drawing.Size(70, 19);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "Tìm kiếm";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -64,18 +69,21 @@
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(21, 30);
+            this.txtSearch.Location = new System.Drawing.Point(16, 24);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(387, 22);
+            this.txtSearch.Size = new System.Drawing.Size(291, 20);
             this.txtSearch.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.radioButton2);
             this.groupBox2.Controls.Add(this.radioButton1);
-            this.groupBox2.Location = new System.Drawing.Point(605, 13);
+            this.groupBox2.Location = new System.Drawing.Point(454, 11);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(288, 74);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(216, 60);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tìm theo";
@@ -83,9 +91,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(169, 29);
+            this.radioButton2.Location = new System.Drawing.Point(127, 24);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(88, 21);
+            this.radioButton2.Size = new System.Drawing.Size(70, 17);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Tên sách";
@@ -94,9 +103,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(16, 30);
+            this.radioButton1.Location = new System.Drawing.Point(12, 24);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(83, 21);
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Mã ISBN";
@@ -105,35 +115,48 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 104);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 84);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(881, 382);
+            this.dataGridView1.Size = new System.Drawing.Size(661, 310);
             this.dataGridView1.TabIndex = 2;
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(818, 492);
+            this.btnCancel.Location = new System.Drawing.Point(614, 400);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(56, 19);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Thoát";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
             // frmSearch
             // 
             this.AcceptButton = this.btnSearch;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(902, 519);
+            this.ClientSize = new System.Drawing.Size(676, 422);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmSearch";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmSearch";
@@ -156,5 +179,6 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ManageLibrary/frmSearch.cs
+++ ManageLibrary/frmSearch.cs
@@ -14,9 +14,10 @@ namespace ManageLibrary
     public partial class frmSearch : Form
     {
         public frmSearch()
-        {         
+        {
             InitializeComponent();
             radioButton1.Checked = true;
+            label1.Visible = false;
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
@@ -24,21 +25,59 @@ namespace ManageLibrary
             BorrowDetailsData bdd = new BorrowDetailsData();
             if (radioButton1.Checked)
             {
-                DataTable dtBook = new DataTable();
-                dtBook = bdd.GetBookListISBNByDataSet(txtSearch.Text).Tables[0];
-                dataGridView1.DataSource = dtBook;
-                dataGridView1.Columns.Remove("Quantity");
-                dataGridView1.Columns.Remove("SLGoc");
-                dataGridView1.Columns.Remove("SLMuon");
+                if (txtSearch.Text != "")
+                {
+                    DataTable dtBook = new DataTable();
+                    dtBook = bdd.GetBookListISBNByDataSet(txtSearch.Text).Tables[0];
+                    int c = bdd.GetBookListISBNByDataSet(txtSearch.Text).Tables[0].Rows.Count;
+                    if (c != 0)
+                    {
+                        dataGridView1.DataSource = dtBook;
+                        dataGridView1.Columns.Remove("Quantity");
+                        dataGridView1.Columns.Remove("SLGoc");
+                        dataGridView1.Columns.Remove("SLMuon");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy!");
+                        txtSearch.Focus();
+                    }
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label1.ForeColor = System.Drawing.Color.Red;
+                    label1.Text = "Không được để trống";
+                    txtSearch.Focus();
+                }
+
             }
             if (radioButton2.Checked)
             {
-                DataTable dtBook = new DataTable();
-                dtBook = bdd.GetBookListNameByDataSet(txtSearch.Text).Tables[0];
-                dataGridView1.DataSource = dtBook;
-                dataGridView1.Columns.Remove("Quantity");
-                dataGridView1.Columns.Remove("SLGoc");
-                dataGridView1.Columns.Remove("SLMuon");
+                if (txtSearch.Text != "")
+                {
+                    DataTable dtBook = new DataTable();
+                    dtBook = bdd.GetBookListNameByDataSet(txtSearch.Text).Tables[0];
+                    int c = bdd.GetBookListNameByDataSet(txtSearch.Text).Tables[0].Rows.Count;
+                    if (c != 0)
+                    {
+                        dataGridView1.DataSource = dtBook;
+                        dataGridView1.Columns.Remove("Quantity");
+                        dataGridView1.Columns.Remove("SLGoc");
+                        dataGridView1.Columns.Remove("SLMuon");
+                    }else
+                    {
+                        MessageBox.Show("Không tìm thấy!");
+                        txtSearch.Focus();
+                    }
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label1.ForeColor = System.Drawing.Color.Red;
+                    label1.Text = "Không được để trống";
+                    txtSearch.Focus();
+                }
             }
         }
 
